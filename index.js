97e629a98c617eb06f4f1a02f829f09bd2335927
alexis romero mendoza <alexis.rm162917@gmail.com>
2023-11-21 12:29:32 -0700

    primera muestra

---
+++ index.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const carousel = document.getElementById('photoCarousel');
+    const totalSlides = carousel.children.length;
+
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+
+    const translateValue = -currentIndex * 100 + '%';
+    carousel.style.transform = 'translateX(' + translateValue + ')';
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
